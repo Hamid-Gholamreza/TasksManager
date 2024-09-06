6fd581ed4962f22687e9e7cf4dded3585407ae05
Hamid <ghhamid80@gmail.com>
2024-09-06 16:52:53 +0330

    installing tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {
+    },
+  },
+
+  daisyui: {
+    darkTheme: 'light', // Use the 'light' theme instead of the default dark theme
+},
+
+  plugins: [],
+};
+
+
