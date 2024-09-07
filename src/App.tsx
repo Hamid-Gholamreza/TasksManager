e7fe78a3f31f4fc70ff70f0ecc7ef5a596e3c590
Hamid <ghhamid80@gmail.com>
2024-09-07 13:52:05 +0330

    fixing the problem of deleting and adding

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,62 @@
 import React, {useState, useEffect} from 'react';
 import './App.css';
 import StickyNote from './components/StickyNote';
+import CloseIcon from '@mui/icons-material/Close';
+import Draggable from 'react-draggable';
 
-interface Note {
+
+export interface Note {
+  id: number,
   title: string,
   message: string,
-  date: Date
+  date: string,
+  isInitial: boolean
 }
 
 
 function App() {
 
+
+  const [newNote, setNewNote] = useState(false);
   const [notesList, setNotesList] = useState<Note[]>(() => {
     const storedNotes = localStorage.getItem('notesList');
     return storedNotes ? JSON.parse(storedNotes).map((note: any) => ({
       ...note,
-      date: new Date(note.date)
     })) : [];
   });
+  const [count, setCount] = useState(notesList?.length);
+
+  const initialNote: Note = {
+    id: count,
+    title: '',
+    message: '',
+    date: '',
+    isInitial: true
+  }
+
+  const handleDelete = (id: number) => {
+    setNotesList((prevNotes) => prevNotes.filter(note => note.id !== id ))
+  }
+
+  const Notes = notesList.map((item: Note, index) => {
+    return (
+        <Draggable>
+            <div className='relative'>
+                <StickyNote key={index} note={item} />
+                <button className='absolute top-0 left-[375px] bg-red-600 rounded-full text-white p-1'
+                onClick={() => handleDelete(item.id)}><CloseIcon/></button>
+            </div>
+        </Draggable>
+    )
+  })
+
 
   const handleAddNote = () => {
-
-    
-
+    setNewNote(true);
+    setCount(count + 1);
+    setNotesList((prevNotes) => [...prevNotes, initialNote]);
+    localStorage.setItem('notesList', JSON.stringify(notesList));
+    setNewNote(false);
   }
 
   useEffect(() => {
@@ -30,17 +64,29 @@ function App() {
   }, [notesList]);
 
 
+
+
+
   return (
     <div className='w-full h-[100vh] relative'>
-
       <div className='w-full h-[200px] bg-blue-600 flex flex-col justify-center items-center gap-8'>
         <h1 className='text-xl text-center'>برای اضافه کردن یادداشت جدید کلیک کنید</h1>
         <button className='w-12 h-12 bg-green-500 rounded-lg text-2xl text-white' onClick={handleAddNote}>+</button>
       </div>
 
       {/* Notes Container */}
-      <div>
-
+      <div className='h-[100vh]'>
+        {
+          newNote ? 
+          <Draggable>
+              <div className='relative'>
+                <StickyNote note={initialNote} />
+                <button className='absolute top-0 left-[375px] bg-red-600 rounded-full text-white p-1'
+                  onClick={() => handleDelete(initialNote.id)}><CloseIcon /></button>
+              </div>
+          </Draggable> : ''
+        }
+          {Notes}
       </div>
     </div>
   );
--- src/components/StickyNote.tsx
+++ src/components/StickyNote.tsx
@@ -1,15 +1,146 @@
-import React, { CSSProperties } from 'react';
+import React, { useState } from 'react';
 import Draggable from 'react-draggable';
+import { Note } from '../App';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+import NotesIcon from '@mui/icons-material/Notes';
+import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive';
+import { Tune } from '@mui/icons-material';
 
-const StickyNote: React.FC = () => {
+const theme = createTheme({
+  components: {
+    MuiInputLabel: {
+      styleOverrides: {
+        root: {
+          right: '0',
+          left: 'auto',
+          transformOrigin: 'top right',
+          textAlign: 'right',
+          width: '100%',
+        },
+        shrink: {
+          transformOrigin: 'top right',
+        },
+      },
+    },
+    MuiInputBase: {
+      styleOverrides: {
+        root: {
+          direction: 'rtl',
+        },
+      },
+    },
+  },
+});
+
+
+interface NoteProps {
+  note: Note;
+}
+
+const StickyNote: React.FC<NoteProps> = ({ note }) => {
+
+  const [open, setOpen] = useState(note.isInitial);
+
+  const [noteData, setNoteData] = useState({
+    id: note.id,
+    title: note.title,
+    message: note.message,
+    date: note.date,
+    isInitial: true
+  })
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    const {name , value} = e.target;
+    setNoteData({...noteData, [name]: value});
+};
+
+  const [notesList, setNotesList] = useState<Note[]>(() => {
+    const storedNotes = localStorage.getItem('notesList');
+    return storedNotes ? JSON.parse(storedNotes).map((note: any) => ({
+      ...note
+    })) : [];
+  });
+
+  const handleOpen = () => {
+    setOpen(!open);
+  }
+
+  const handleEdit = () => {
+    handleOpen();
+    console.log('is open');
+  }
+
+
   return (
     <Draggable>
-      <div className='w-[400px] h-[180px] bg-white cursor-move p-10 rounded-lg shadow-2xl'>
-        <h1>title</h1>
-        <textarea  placeholder="یادداشت خود را اینجا بنویسید..."
-        className='w-full h-full border-none outline-none resize-none bg-transparent' dir='rtl'>
-        </textarea>
-      </div>
+      {open === true ? 
+            <div className='w-[400px] h-fit bg-white cursor-move p-10 rounded-lg shadow-2xl'>
+                <ThemeProvider theme={theme}>
+                    <Box
+                      component="form"
+                      sx={{ '& > :not(style)': { m: 1, width: '25ch' } }}
+                      noValidate
+                      autoComplete="off"
+                      className='flex flex-col justify-center items-center'
+                    >
+                      <TextField
+                        id="standard-basic"
+                        label="عنوان"
+                        variant="standard"
+                        dir="rtl"
+                        className="float-right"
+                        name='title'
+                        value={noteData.title}
+                        onChange={handleInputChange}
+                      />
+                      <TextField
+                        id="standard-multiline-flexible"
+                        label="یادداشت"
+                        multiline
+                        maxRows={4}
+                        variant="standard"
+                        name='message'
+                        value={noteData.message}
+                        onChange={handleInputChange}
+                      />
+                      <TextField
+                        id="standard-basic"
+                        label="عنوان"
+                        variant="standard"
+                        dir="rtl"
+                        className="float-right"
+                        name='date'
+                        value={noteData.date}
+                        onChange={handleInputChange}
+                      />
+                    </Box>
+                    <div className='w-full flex justify-center items-center gap-5 mt-3'>
+                      <button className='w-[100px] flex justify-center items-center bg-blue-500 text-black text-xs p-1 rounded-2xl
+                      hover:text-white hover:bg-blue-700' onClick={handleEdit}>
+                          <EditIcon/>
+                          <p>ویرایش</p>
+                      </button>
+                    </div>
+                </ThemeProvider>
+            </div>
+          : <div onDoubleClick={handleOpen} className='w-[240px] h-[120px] bg-white rounded-2xl
+          border-2 border-gray-300'>
+              <div className='pr-3 pt-2'>
+                <div className='flex flex-row-reverse gap-1 mt-1 text-sm'>
+                  <NotificationsActiveIcon />
+                  <p className='w-full text-right'>{noteData.title}</p>
+                </div>
+                <div className='flex flex-row-reverse gap-1 mt-2 text-sm'>
+                    <NotesIcon />
+                    <p>{noteData.message}</p>
+                </div>
+              </div>
+            </div>
+          }
     </Draggable>
   );
 };
